9cd3a103972d7cc4581ae81e08fb9df9efee42af
Prashant <aoiokido123@gmail.com>
2026-05-17 21:16:39 +0545

    update

--- Server/server.js
+++ Server/server.js
@@ -1,25 +1,24 @@
 require("dotenv").config();
 
-
-const express= require('express');
+const express = require('express');
 const connectDB = require("./config/db");
-const newsRoute=require("./routes/newsRoute")
-const adminRoute=require("./routes/adminRoute")
-const app = express();
+const newsRoute = require("./routes/newsRoute");
+const adminRoute = require("./routes/adminRoute");
 const cors = require('cors');
+
+const app = express();
+
 app.use(cors({
-  origin: 'http://localhost:3000'
+  origin: process.env.CLIENT_URL || 'http://localhost:3000'
 }));
-app.use(express.json())
+
+app.use(express.json());
 connectDB();
 
-
-
-app.use('/api/news',newsRoute)
+app.use('/api/news', newsRoute);
 app.use("/api/admin", adminRoute);
 
-
-const PORT=process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server running on port http://localhost:${PORT}`);
+  console.log(`Server running on port http://localhost:${PORT}`);
 });
